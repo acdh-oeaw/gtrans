9e76919da5a0fe87b662ca93a336b06092cc25ec
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2019-10-07 11:57:06 +0200

    data view for all words added

--- wordcloud/urls.py
+++ wordcloud/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "wordcloud"
 
 urlpatterns = [
+    path('all-words/<app_name>/<model_name>/<field_name>', views.word_data_class, name='all_words'),
     path('words/<app_name>/<model_name>/<pk>/<field_name>', views.word_data, name='words'),
 ]
--- wordcloud/views.py
+++ wordcloud/views.py
@@ -5,6 +5,20 @@ from django.http import JsonResponse
 from django.contrib.contenttypes.models import ContentType
 
 
+def word_data_class(request, app_name, model_name, field_name):
+    try:
+        ct = ContentType.objects.get(app_label=app_name, model=model_name)
+    except ObjectDoesNotExist:
+        return JsonResponse({})
+    my_model_class = ct.model_class()
+    items = list(my_model_class.objects.all().values(field_name))
+    text = Counter(" ".join([x[field_name] for x in items]).split(' '))
+    data = [
+        {'name': x[0], 'weight': x[1]} for x in text.items()
+    ]
+    return JsonResponse(data, safe=False)
+
+
 def word_data(request, app_name, model_name, pk, field_name):
     try:
         ct = ContentType.objects.get(app_label=app_name, model=model_name)
