b0367f558a81bb87daddcd6f75cbbd1a995a6389
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2019-10-27 21:14:42 +0100

    minor

--- archiv/serializers.py
+++ archiv/serializers.py
@@ -6,10 +6,10 @@ from vocabs.serializers import *
 
 class ArchResourceSerializer(serializers.HyperlinkedModelSerializer):
 
-    # mentioned_person = PersonSerializer(many=True, read_only=True)
-    # mentioned_inst = InstitutionSerializer(many=True, read_only=True)
-    # mentioned_place = PlaceSerializer(many=True, read_only=True)
-    # subject_norm = SkosConceptSerializer(many=True, read_only=True)
+    mentioned_person = PersonSerializer(many=True, read_only=True)
+    mentioned_inst = InstitutionSerializer(many=True, read_only=True)
+    mentioned_place = PlaceSerializer(many=True, read_only=True)
+    subject_norm = SkosConceptSerializer(many=True, read_only=True)
     tei_doc = serializers.SerializerMethodField()
 
     class Meta:
